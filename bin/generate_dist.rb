f4eb55926113ae45bfae7e2555d32129c79f9f6c
Jesse McGinnis <accounts@jcmcginnis.com>
2024-01-29 17:35:50 -0500

    Update generate_dist.rb
    It'll be easier for LLMs with ` : ` vs `: `

--- bin/generate_dist.rb
+++ bin/generate_dist.rb
@@ -12,7 +12,7 @@ class GenerateDist
       Dir.glob('src/shopify/categories/*.yml').each do |file|
         file_name = File.basename(file, ".yml")
         data = YAML.load_file(file)
-        categories[file_name] = data.map{ "#{_1["public_id"]}: #{_1["fully_qualified_type"]}"}
+        categories[file_name] = data.map{ "#{_1["public_id"]} : #{_1["fully_qualified_type"]}"}
       end
 
       categories.each do |filename, data|
