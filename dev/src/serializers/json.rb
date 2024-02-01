3379e122a14f355093d468569aed03d746428b1e
Jesse McGinnis <accounts@jcmcginnis.com>
2024-02-01 17:02:00 -0500

    Add taxonomy.json (and generators thereof)

--- dev/src/serializers/json.rb
+++ dev/src/serializers/json.rb
@@ -2,17 +2,31 @@ require 'json'
 
 module Serializers
   class JSON
-    attr_reader :taxonomy, :version
-
     def initialize(taxonomy, version)
       @taxonomy = taxonomy
       @version = version
     end
 
+    def taxonomy
+      output = {
+        version: @version,
+        verticals: @taxonomy.verticals.map do |vertical|
+          root = vertical.first
+          {
+            name: root.name,
+            prefix: root.id.downcase,
+            categories: vertical.map(&:serialize_as_hash),
+          }
+        end,
+        attributes: @taxonomy.attributes.map(&:serialize_as_hash),
+      }
+      ::JSON.pretty_generate(output)
+    end
+
     def categories
       output = {
-        version: version,
-        verticals: taxonomy.verticals.map do |vertical|
+        version: @version,
+        verticals: @taxonomy.verticals.map do |vertical|
           root = vertical.first
           {
             name: root.name,
@@ -25,7 +39,7 @@ module Serializers
     end
 
     def attributes
-      output = taxonomy.attributes.map(&:serialize_as_hash)
+      output = @taxonomy.attributes.map(&:serialize_as_hash)
       ::JSON.pretty_generate(output)
     end
   end
--- dev/src/serializers/text.rb
+++ dev/src/serializers/text.rb
@@ -1,15 +1,13 @@
 module Serializers
   class Text
-    attr_reader :taxonomy, :version
-
     def initialize(taxonomy, version)
       @taxonomy = taxonomy
       @version = version
     end
 
     def categories
-      header = "# Shopify Product Taxonomy: #{Date.today} (#{version})"
-      taxonomy.categories.map(&:serialize_as_txt).unshift(header).join("\n")
+      header = "# Shopify Product Taxonomy: #{@version}"
+      @taxonomy.categories.map(&:serialize_as_txt).unshift(header).join("\n")
     end
   end
 end
