f65e336f7a771c9d45e94d93a6c8adfd8bbeacca
Jesse McGinnis <accounts@jcmcginnis.com>
2024-02-28 08:14:48 -0500

    Add an attributes.txt

--- dev/app/serializers/dist/text.rb
+++ dev/app/serializers/dist/text.rb
@@ -1,13 +1,14 @@
 module Serializers
   module Dist
     class Text
-      def initialize(verticals:, version:)
+      def initialize(verticals:, properties:, version:)
         @verticals = verticals
+        @properties = properties
         @version = version
       end
 
       def categories
-        header = "# Shopify Product Taxonomy: #{@version}"
+        header = "# Shopify Product Taxonomy - Categories: #{@version}"
         gid_padd = Category.reorder("LENGTH(id) desc").first.gid.size
         @verticals
           .flat_map(&:descendants_and_self)
@@ -16,10 +17,23 @@ module Serializers
           .join("\n")
       end
 
+      def attributes
+        header = "# Shopify Product Taxonomy - Attributes: #{@version}"
+        gid_padd = Property.reorder("LENGTH(id) desc").first.gid.size
+        @properties
+          .map { serialize_property(_1, gid_padd:) }
+          .unshift(header)
+          .join("\n")
+      end
+
       private
 
       def serialize_category(category, gid_padd:)
         "#{category.gid.ljust(gid_padd)} : #{category.full_name}"
+      end
+
+      def serialize_property(property, gid_padd:)
+        "#{property.gid.ljust(gid_padd)} : #{property.name}"
       end
     end
   end
