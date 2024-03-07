14a9a30687ae7b1b2bb36466127cbbe3cc3f2cbd
Jesse McGinnis <accounts@jcmcginnis.com>
2024-03-07 08:58:43 -0500

    Add factory method to PropertyValueTest

--- dev/test/unit/property_value_test.rb
+++ dev/test/unit/property_value_test.rb
@@ -6,14 +6,24 @@ class PropertyValueTest < Minitest::Test
   end
 
   def test_gid
-    assert_equal 'gid://shopify/Taxonomy/Value/12', PropertyValue.new(id: 12, name: 'Foo').gid
+    assert_equal 'gid://shopify/Taxonomy/Value/12', value(id: 12).gid
   end
 
   def test_default_ordering_places_other_at_end
-    red = PropertyValue.create(id: 11, name: 'Red')
-    zoo = PropertyValue.create(id: 12, name: 'Zoo')
-    other = PropertyValue.create(id: 13, name: 'Other')
+    red = value!(id: 11, name: 'Red')
+    zoo = value!(id: 12, name: 'Zoo')
+    other = value!(id: 13, name: 'Other')
 
     assert_equal [red, zoo, other], PropertyValue.all.to_a
+  end
+
+  private
+
+  def value(id: 1, name: 'Foo')
+    PropertyValue.new(id: id, name: name)
+  end
+
+  def value!(id: 1, name: 'Foo')
+    PropertyValue.create(id: id, name: name)
   end
 end
