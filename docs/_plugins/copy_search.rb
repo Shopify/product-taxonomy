e5a05001a1e728861aea413732f21c24fa2d87d0
Jesse McGinnis <accounts@jcmcginnis.com>
2024-03-20 13:17:39 -0400

    Index copy happens in GH Action since GH rune Jekyll build in safe mode

--- docs/_plugins/copy_search.rb
+++ docs/_plugins/copy_search.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
+# this doesn't run on GitHub Pages, hence the bin command
 root = File.expand_path("../..", __dir__)
-
 Jekyll::Hooks.register(:site, :post_write) do
-  Dir.glob("#{root}/docs/_data/*/").each do |dir|
-    src = "#{dir}search_index.json"
-    target = "#{root}/_site/releases/#{File.basename(dir)}/search_index.json"
-    puts "Copying #{src} to #{target}"
-    FileUtils.cp(src, target)
-  end
+  system("#{root}/bin/copy_docs_search_indexes")
 end
