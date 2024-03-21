957f3337e6d8504c68ccfbe15d9eb988be4c7607
Jesse McGinnis <accounts@jcmcginnis.com>
2024-03-21 08:58:36 -0400

    Make models for join tables

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,14 @@ class Category < ApplicationRecord
 
   has_many :children, class_name: "Category", inverse_of: :parent
   belongs_to :parent, class_name: "Category", optional: true
-  has_and_belongs_to_many :properties
+
+  has_and_belongs_to_many :properties,
+    join_table: :categories_properties,
+    foreign_key: :category_id,
+    association_foreign_key: :property_friendly_id
+  def property_friendly_ids=(ids)
+    self.properties = Property.where(friendly_id: ids)
+  end
 
   validates :id,
     presence: { strict: true },
