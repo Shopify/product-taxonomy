bea880e431aa7eddc2b5568395c8764ded9d6837
Jesse McGinnis <accounts@jcmcginnis.com>
2024-03-21 08:21:25 -0400

    Use Minitest::Hooks to split up integration tests

--- test/integration/all_data_files_import_test.rb
+++ test/integration/all_data_files_import_test.rb
@@ -3,37 +3,56 @@
 require_relative "../test_helper"
 
 class AllDataFilesImportTest < ActiveSupport::TestCase
-  def teardown
-    Category.destroy_all
-    Property.destroy_all
-    PropertyValue.destroy_all
+  include Minitest::Hooks
+
+  def before_all
+    @raw_attributes_data = YAML.load_file("#{Application.root}/data/attributes/attributes.yml")
+    DB::Seed.attributes_from(@raw_attributes_data)
+
+    # Categories are only successfully parseable if attributes are already present
+    category_files = Dir.glob("#{Application.root}/data/categories/*.yml")
+    @raw_verticals_data = category_files.map { YAML.load_file(_1) }
+    DB::Seed.categories_from(@raw_verticals_data)
   end
 
-  test "DB::Seed imports everything from all data/ files" do
-    raw_attributes_data = YAML.load_file("#{Application.root}/data/attributes/attributes.yml")
-    DB::Seed.attributes_from(raw_attributes_data)
+  test "Attributes are correctly imported from attributes.yml" do
+    assert_equal @raw_attributes_data.size, Property.count
+  end
 
-    assert_equal raw_attributes_data.size, Property.count
-    raw_attributes_data.each do |raw_attribute|
+  test "Attributes are consistent with attributes.yml" do
+    @raw_attributes_data.each do |raw_attribute|
       deserialized_attribute = Serializers::Data::PropertySerializer.deserialize(raw_attribute)
       real_attribute = Property.find(raw_attribute.fetch("id"))
 
       assert_equal deserialized_attribute, real_attribute
     end
+  end
 
-    category_files = Dir.glob("#{Application.root}/data/categories/*.yml")
-    raw_verticals_data = category_files.map { YAML.load_file(_1) }
-    DB::Seed.categories_from(raw_verticals_data)
+  test "Attributes are all valid" do
+    Property.all.each do |attribute|
+      assert_predicate attribute, :valid?
+    end
+  end
 
-    assert_equal raw_verticals_data.size, Category.verticals.count
-    assert_equal raw_verticals_data.map(&:size).sum, Category.count
-    raw_verticals_data.flatten.each do |raw_category|
+  test "Categories are fully imported from categories/*.yml" do
+    assert_equal @raw_verticals_data.size, Category.verticals.count
+    assert_equal @raw_verticals_data.map(&:size).sum, Category.count
+  end
+
+  test "Categories are consistent with categories/*.yml" do
+    @raw_verticals_data.flatten.each do |raw_category|
       deserialized_category = Serializers::Data::CategorySerializer.deserialize(raw_category)
       real_category = Category.find(raw_category.fetch("id"))
 
       assert_equal deserialized_category, real_category
       assert_equal raw_category.fetch("children").size, real_category.children.count
       assert_equal deserialized_category.children, real_category.children
+    end
+  end
+
+  test "Categories are all valid" do
+    Category.all.each do |category|
+      assert_predicate category, :valid?
     end
   end
 end
