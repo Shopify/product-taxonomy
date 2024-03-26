8d934f1d9b09306bbe6da3831ace722e6c273705
Hongming Wang <hongming.wang@shopify.com>
2024-03-25 21:45:22 -0400

    Generate inline taxonomy mapping in taxonomy.json

--- app/services/mapping_builder.rb
+++ app/services/mapping_builder.rb
@@ -2,6 +2,16 @@
 
 class MappingBuilder
   class << self
+    def build_one_to_one_mappings_for_vertical(mapping_rules:, vertical:)
+      relevant_rules = mapping_rules.select { |rule| rule.input.product_category_id.start_with?(vertical.id) }
+      relevant_rules.map do |rule|
+        {
+          input: rule.input.payload.delete_if { |_k, v| v.nil? },
+          output: rule.output.payload.delete_if { |_k, v| v.nil? },
+        }
+      end
+    end
+
     def build_mappings_for_vertical(mapping_rules:, vertical:)
       puts " → for #{Integration.find(mapping_rules.first.integration_id).name} in #{vertical.name}..."
       relevant_rules = mapping_rules.select { |rule| rule.input.product_category_id.start_with?(vertical.id) }
