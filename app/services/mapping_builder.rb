fbb4dd2e36f4682a92fbfff6987ea4111e658ec2
Hongming Wang <hongming.wang@shopify.com>
2024-04-18 17:41:19 -0400

    Code cleanup

--- app/services/mapping_builder.rb
+++ app/services/mapping_builder.rb
@@ -11,47 +11,5 @@ class MappingBuilder
           }
         end
     end
-
-    def build_category_to_category_mappings_for_vertical(mapping_rules:, vertical:)
-      mapping_rules
-        .select { _1.input.product_category_id.start_with?(vertical.id) }
-        .map do
-          {
-            input: _1.input.payload.delete_if { |_k, v| v.nil? },
-            output: _1.output.payload.delete_if { |_k, v| v.nil? },
-          }
-        end
-    end
-
-    def build_mappings_for_vertical(mapping_rules:, vertical:)
-      relevant_rules = mapping_rules.select { |rule| rule.input.product_category_id.start_with?(vertical.id) }
-      return if relevant_rules.count.zero?
-
-      inputs = InputProductGenerator.generate_inputs_for_categories(vertical.descendants_and_self, relevant_rules)
-
-      build_mappings_from_inputs_and_rules(inputs, relevant_rules)
-    end
-
-    private
-
-    def build_mappings_from_inputs_and_rules(inputs, rules)
-      inputs.map do |input|
-        mapping = {
-          input: input,
-          output: rules.filter { |rule| rule.match?(input) }.reduce({}) do |final_output, rule|
-            rule.apply(final_output)
-          end,
-        }
-
-        if mapping[:output].empty?
-          nil
-        elsif mapping[:input][:attributes].empty?
-          mapping[:input].delete(:attributes)
-          mapping
-        else
-          mapping
-        end
-      end
-    end
   end
 end
