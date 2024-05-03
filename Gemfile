9b7783aea0d0d894dd64b84c98ca10ea3bc3003c
Jesse McGinnis <accounts@jcmcginnis.com>
2024-05-03 18:25:18 -0400

    Change minitest to minitest/rails for DB cleanup

--- Gemfile
+++ Gemfile
@@ -14,5 +14,6 @@ gem "rubocop-shopify", require: false
 
 group :test do
   gem "minitest", "~> 5.21"
+  gem "minitest-rails", "~> 7.1"
   gem "minitest-hooks", "~> 1.5"
 end
---
+++ test/unit/serializers/source/categories_property_serializer_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "../../../test_helper"
+
+module Serializers
+  module Source
+    class CategoriesPropertySerializerTest < ActiveSupport::TestCase
+      test "foo" do
+        assert(true)
+      end
+    end
+  end
+end
