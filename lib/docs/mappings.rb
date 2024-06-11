cdf34535ef5e9e6be9d8705a545fdb0472bba81e
Alex Grant <alex.grant@shopify.com>
2024-06-11 16:39:15 -0400

    also reverse input & ouput ver for shopify

--- lib/docs/mappings.rb
+++ lib/docs/mappings.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 module Docs
   class Mappings
     def reverse_shopify_mapping_rules(mappings)
       mappings.each do |mapping|
         if shopify_mapping?(mapping)
+          reverse_taxonomy_names(mapping)
           reverse_mapping_rules(mapping)
         end
       end
@@ -12,12 +15,20 @@ module Docs
       mapping["rules"].each do |rule|
         rule["output"]["category"].each_with_index do |output, index|
           if index == 0
-            rule["input"], rule["output"] = { "category" => output }, {"category" => [rule["input"]["category"]]}
+            rule["output"] = { "category" => [rule["input"]["category"]] }
+            rule["input"] = { "category" => output }
           else
             mapping.push = build_rule(input: output, output: rule["input"]["category"])
           end
         end
       end
+    end
+
+    def reverse_taxonomy_names(mapping)
+      input = mapping["input_taxonomy"]
+      output = mapping["output_taxonomy"]
+      mapping["input_taxonomy"] = output
+      mapping["output_taxonomy"] = input
     end
 
     def shopify_mapping?(mapping)
@@ -26,8 +37,8 @@ module Docs
 
     def build_rule(input:, output:)
       {
-        "input" => {"category" => input},
-        "output" => {"category" => [output]}
+        "input" => { "category" => input },
+        "output" => { "category" => [output] },
       }
     end
   end
