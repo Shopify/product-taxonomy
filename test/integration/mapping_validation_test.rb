deb7b7ec3db6da7cf9b65ca5f50a08bb20b0c71d
Catherine Fu <catherine.fu@shopify.com>
2024-07-18 17:54:22 -0400

    check which shopify category IDs lack corresponding channel mappings

--- test/integration/mapping_validation_test.rb
+++ test/integration/mapping_validation_test.rb
@@ -19,6 +19,34 @@ class MappingValidationTest < ActiveSupport::TestCase
       #{invalid_categories}"
   end
 
+  test "every Shopify category has corresponding channel mappings" do
+    mappings_json_data = CLI.new.parse_json("dist/en/integrations/all_mappings.json")
+    shopify_category_lack_mappings = []
+
+    mappings_json_data["mappings"].each do |mapping|
+      next unless mapping["input_taxonomy"].include?("shopify")
+
+      all_shopify_category_ids = category_ids_from_taxonomy(mapping["input_taxonomy"])
+      next if all_shopify_category_ids.nil?
+
+      shopify_category_ids_from_mappings_input = Set.new
+      mapping["rules"].each do |rule|
+        shopify_category_ids_from_mappings_input.add(rule["input"]["category"]["id"])
+      end
+      shopify_category_ids_diff = all_shopify_category_ids - shopify_category_ids_from_mappings_input
+
+      shopify_category_lack_mappings << {
+        input_taxonomy: mapping["input_taxonomy"],
+        output_taxonomy: mapping["output_taxonomy"],
+        shopify_category_ids_lack_mappings: shopify_category_ids_diff,
+      }
+    end
+
+    assert shopify_category_lack_mappings.empty?,
+      "The following shopify category ids lack corresponding channel mappings:
+    #{shopify_category_lack_mappings}"
+  end
+
   def validate_mapping_category_ids(mapping_rules, input_or_output, input_or_output_taxonomy)
     category_ids = category_ids_from_taxonomy(input_or_output_taxonomy)
 
