59edfecba3863cd1f03cc2c5e00e62852a5b11d6
Catherine Fu <catherine.fu@shopify.com>
2024-07-22 18:28:45 -0400

    addressed comments and improved the output format

--- test/integration/mapping_validation_test.rb
+++ test/integration/mapping_validation_test.rb
@@ -3,10 +3,14 @@
 require_relative "../test_helper"
 
 class MappingValidationTest < ActiveSupport::TestCase
+  include Minitest::Hooks
+  def setup
+    @mappings_json_data = CLI.new.parse_json("dist/en/integrations/all_mappings.json")
+  end
+
   test "category IDs in mappings are valid" do
-    mappings_json_data = System.new.parse_json("dist/en/integrations/all_mappings.json")
     invalid_categories = []
-    mappings_json_data["mappings"].each do |mapping|
+    @mappings_json_data["mappings"].each do |mapping|
       res = validate_mapping_category_ids(mapping["rules"], "input", mapping["input_taxonomy"])
       invalid_categories.concat(res)
 
@@ -20,32 +24,39 @@ class MappingValidationTest < ActiveSupport::TestCase
   end
 
   test "every Shopify category has corresponding channel mappings" do
-    mappings_json_data = CLI.new.parse_json("dist/en/integrations/all_mappings.json")
-    shopify_category_lack_mappings = []
-
-    mappings_json_data["mappings"].each do |mapping|
+    shopify_categories_lack_mappings = []
+    @mappings_json_data["mappings"].each do |mapping|
       next unless mapping["input_taxonomy"].include?("shopify")
 
       all_shopify_category_ids = category_ids_from_taxonomy(mapping["input_taxonomy"])
       next if all_shopify_category_ids.nil?
 
-      shopify_category_ids_from_mappings_input = Set.new
-      mapping["rules"].each do |rule|
-        shopify_category_ids_from_mappings_input.add(rule["input"]["category"]["id"])
-      end
-      shopify_category_ids_diff = all_shopify_category_ids - shopify_category_ids_from_mappings_input
+      shopify_category_ids_from_mappings_input = mapping["rules"]
+        .map { _1.dig("input", "category", "id") }
+        .to_set
 
-      shopify_category_lack_mappings << {
+      unmapped_category_ids = all_shopify_category_ids - shopify_category_ids_from_mappings_input
+
+      next if unmapped_category_ids.empty?
+
+      shopify_categories_lack_mappings << {
         input_taxonomy: mapping["input_taxonomy"],
         output_taxonomy: mapping["output_taxonomy"],
-        count_of_shopify_category_ids_lack_mappings: shopify_category_ids_diff.size,
-        shopify_category_ids_lack_mappings: shopify_category_ids_diff,
+        unmapped_category_ids: unmapped_category_ids,
       }
     end
 
-    assert shopify_category_lack_mappings.empty?,
-      "The following shopify category ids lack corresponding channel mappings:
-    #{shopify_category_lack_mappings}"
+    unless shopify_categories_lack_mappings.empty?
+      puts "Shopify Categories are missing mappings for the following integrations:"
+      shopify_categories_lack_mappings.each_with_index do |mapping, index|
+        puts ""
+        puts "[#{index + 1}] #{mapping[:input_taxonomy]} to #{mapping[:output_taxonomy]} (#{mapping[:unmapped_category_ids].size} missing)"
+        mapping[:unmapped_category_ids].each do |category_id|
+          puts " - #{category_id}"
+        end
+      end
+      assert(shopify_categories_lack_mappings.empty?, "Shopify Categories are missing mappings.")
+    end
   end
 
   def validate_mapping_category_ids(mapping_rules, input_or_output, input_or_output_taxonomy)
