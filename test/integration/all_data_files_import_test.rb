6c5238b73c0b00bd54443bdc652ff57206f665ce
Jesse McGinnis <accounts@jcmcginnis.com>
2024-07-23 18:21:05 -0400

    Move CLI to lib

--- test/integration/all_data_files_import_test.rb
+++ test/integration/all_data_files_import_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../test_helper"
-require_relative "../../config/cli"
+require_relative "../../lib/cli"
 require_relative "../../db/seed"
 
 class AllDataFilesImportTest < ActiveSupport::TestCase
