5d59fe79c845ecfaadab8d85bda5a6cc8e7b5bf7
Jesse McGinnis <accounts@jcmcginnis.com>
2024-08-02 19:09:02 -0400

    Convert bin/data/dump_categories to Command style

---
+++ app/commands/source/dump_vertical_command.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Source
+  class DumpVerticalCommand < ApplicationCommand
+    usage do
+      no_command
+    end
+
+    keyword :verticals do
+      desc "Verticals to export to data/"
+      convert :list
+      default -> { Category.verticals.map(&:id).join(",") }
+      validate -> { Category.verticals.map(&:id).include?(_1) }
+    end
+
+    def execute
+      frame("Dumping #{params[:verticals].size} verticals") do
+        params[:verticals].each do |vertical_id|
+          update_vertical_file(vertical_id)
+        end
+      end
+    end
+
+    private
+
+    def update_vertical_file(vertical_id)
+      vertical = Category.find_by!(id: vertical_id)
+      spinner("Updating `#{vertical.name}`") do |sp|
+        path = "data/categories/#{vertical.handleized_name}.yml"
+        sys.write_file!(path) do |file|
+          file.write(vertical.as_json_for_data_with_descendants.to_yaml)
+        end
+        sp.update_title("Updated `#{path}`")
+      end
+    end
+  end
+end
