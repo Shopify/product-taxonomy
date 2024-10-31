24bec39025effe86675cc7514c6f768956d43073
Daniel Gross <daniel.gross@shopify.com>
2024-10-31 10:15:40 -0400

    Start from gem template

---
+++ lib/product_taxonomy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "product_taxonomy/version"
+
+module ProductTaxonomy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/product_taxonomy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ProductTaxonomy
+  VERSION = "0.1.0"
+end
