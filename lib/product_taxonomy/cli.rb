6b13424cab52e31f19390f644945f1432f7ad280
Daniel Gross <daniel.gross@shopify.com>
2024-10-31 15:04:19 -0400

    Remove leftovers

--- lib/product_taxonomy/cli.rb
+++ lib/product_taxonomy/cli.rb
@@ -23,34 +23,6 @@ module ProductTaxonomy
         end
         puts "Elapsed time: #{elapsed_time} seconds"
       end
-
-      def build_tree(yaml_data, parent = nil)
-        nodes = {}
-
-        # First pass: Create all nodes
-        yaml_data.each do |item|
-          node = Node.new(item["id"], item["name"], item["attributes"])
-          nodes[item["id"]] = node
-          parent&.add_child(node)
-        end
-
-        # Second pass: Build relationships
-        yaml_data.each do |item|
-          parent = nodes[item["id"]]
-          item["children"]&.each do |child_id|
-            child = nodes[child_id]
-            parent.add_child(child) if child
-          end
-        end
-
-        # Return the first node if no parent was provided
-        parent ? parent : nodes[yaml_data.first["id"]]
-      end
-
-      def print_tree(node, level = 0)
-        puts "#{" " * level}#{node.id}: #{node.name}"
-        node.children.each { |child| print_tree(child, level + 2) }
-      end
     end
   end
 end
