ebd92e1c14ddf82dfa68286d55ce212311a17bdf
Daniel Gross <daniel.gross@shopify.com>
2024-11-18 15:29:14 -0500

    Refactor ModelIndex to use hashes instead of sets

--- dev/lib/product_taxonomy/models/model_index.rb
+++ dev/lib/product_taxonomy/models/model_index.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ProductTaxonomy
-  # High-performance in-memory container for a collection of models that uses Hash and Set to support fast uniqueness
+  # High-performance in-memory container for a collection of models that uses hashes to support fast uniqueness
   # checks and lookups by field value.
   class ModelIndex
     # Validator that checks for uniqueness of a field value across all models in a given ModelIndex. Requires the
@@ -11,7 +11,7 @@ module ProductTaxonomy
         context = record.uniqueness_context
         return if context.blank?
 
-        if context.exists?(attribute => value)
+        if context.duplicate?(model: record, field: attribute)
           record.errors.add(attribute, :taken, message: "\"#{value}\" has already been used")
         end
       end
@@ -19,11 +19,9 @@ module ProductTaxonomy
 
     # @param model_class [Class] The class of models to index. Used to determine the fields on which to support
     #   uniqueness checks.
-    # @param hashed_by [Array<String>] The fields by which to hash the models for fast lookups by field value.
-    def initialize(model_class, hashed_by: [])
-      @hashed_models = Array(hashed_by).each_with_object({}) { |field, hash| hash[field] = {} }
+    def initialize(model_class)
       @unique_fields = unique_fields(model_class)
-      @unique_fields_seen = @unique_fields.each_with_object({}) { |field, hash| hash[field] = Set.new }
+      @hashed_models = @unique_fields.each_with_object({}) { |field, hash| hash[field] = {} }
     end
 
     # Add a model to the index.
@@ -31,23 +29,23 @@ module ProductTaxonomy
     # @param model [Object] The model to add to the index.
     def add(model)
       @hashed_models.keys.each do |field|
-        @hashed_models[field][model.send(field)] = model
-      end
-      @unique_fields.each do |field|
-        @unique_fields_seen[field] << model.send(field)
+        @hashed_models[field][model.send(field)] ||= model
       end
     end
 
-    # Check if a field value exists across all models in the index.
+    # Check if a field value is a duplicate of an existing model. A model is considered to be a duplicate if it was
+    # added after the first model with that value.
     #
-    # @param field_value_pair [Hash<Symbol, Object>] A hash with a single key-value pair for the field and value to
-    #   check for existence.
-    # @return [Boolean] Whether the value exists.
-    def exists?(field_value_pair)
-      field, value = field_value_pair.first
-      raise ArgumentError, "Field not indexed for uniqueness: #{field}" unless @unique_fields.include?(field)
+    # @param model [Object] The model to check for uniqueness.
+    # @param field [Symbol] The field to check for uniqueness.
+    # @return [Boolean] Whether the value is unique.
+    def duplicate?(model:, field:)
+      raise ArgumentError, "Field not hashed: #{field}" unless @hashed_models.key?(field)
 
-      @unique_fields_seen[field].include?(value)
+      existing_model = @hashed_models[field][model.send(field)]
+      return false if existing_model.nil?
+
+      model != existing_model
     end
 
     # Get the hash of models indexed by a given field. Only works for fields that were specified as hashed fields when
