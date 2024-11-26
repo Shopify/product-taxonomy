57cdeefde09390d6a9fa2e9029a940240d6090c5
Daniel Gross <daniel.gross@shopify.com>
2024-11-26 12:03:59 -0500

    Add `dist` command to generate distribution files

---
+++ dev/lib/product_taxonomy/commands/command.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "logger"
+require "benchmark"
+
+module ProductTaxonomy
+  class Command
+    attr_reader :logger, :options
+
+    def initialize(options)
+      @options = options
+      @logger = Logger.new($stdout, level: :info)
+      @logger.formatter = proc { |_, _, _, msg| "#{msg}\n" }
+      @logger.level = :debug if options[:verbose]
+      @logger.level = :error if options[:quiet]
+    end
+
+    def run
+      elapsed = Benchmark.realtime do
+        execute
+      end
+      logger.info("Completed in #{elapsed.round(2)} seconds")
+    end
+
+    def execute
+      raise NotImplementedError, "#{self.class}#execute must be implemented"
+    end
+
+    def load_taxonomy
+      ProductTaxonomy::Value.load_from_source(YAML.load_file(File.expand_path(
+        "values.yml",
+        ProductTaxonomy::DATA_PATH,
+      )))
+      ProductTaxonomy::Attribute.load_from_source(YAML.load_file(File.expand_path(
+        "attributes.yml",
+        ProductTaxonomy::DATA_PATH,
+      )))
+
+      glob = Dir.glob(File.expand_path("categories/*.yml", ProductTaxonomy::DATA_PATH))
+      categories_source_data = glob.each_with_object([]) do |file, array|
+        array.concat(YAML.safe_load_file(file))
+      end
+      ProductTaxonomy::Category.load_from_source(categories_source_data)
+    end
+  end
+end
