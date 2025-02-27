a65a4cedce36775f9a12f3517ee60421977829ff
Daniel Gross <daniel.gross@shopify.com>
2025-02-27 15:13:53 -0500

    Improve `Indexed` behaviour with model subclasses

--- dev/lib/product_taxonomy/models/mixins/indexed.rb
+++ dev/lib/product_taxonomy/models/mixins/indexed.rb
@@ -3,6 +3,14 @@
 module ProductTaxonomy
   # Mixin providing indexing for a model, using hashes to support fast uniqueness checks and lookups by field value.
   module Indexed
+    class << self
+      # Keep track of which model class used "extend Indexed" so that the model can be subclassed while still storing
+      # model objects in a shared index on the superclass.
+      def extended(indexed_model_class)
+        indexed_model_class.instance_variable_set(:@is_indexed, true)
+      end
+    end
+
     # Validator that checks for uniqueness of a field value across all models in a given index.
     class UniquenessValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
@@ -19,6 +27,18 @@ module ProductTaxonomy
       hashed_models.keys.each do |field|
         hashed_models[field][model.send(field)] ||= model
       end
+    end
+
+    # Convenience method to create a model, validate it, and add it to the index. If the model is not valid, raise an
+    # error.
+    #
+    # @param args [Array] The arguments to pass to the model's constructor.
+    # @return [Object] The created model.
+    def create_validate_and_add!(...)
+      model = new(...)
+      model.validate!
+      add(model)
+      model
     end
 
     # Check if a field value is a duplicate of an existing model. A model is considered to be a duplicate if it was
@@ -71,6 +91,18 @@ module ProductTaxonomy
       all.size
     end
 
+    # Get the hash of hash of models, indexed by fields marked unique.
+    #
+    # @return [Hash] The hash of hash of models indexed by the fields marked unique.
+    def hashed_models
+      # If we're a subclass of the model class that originally extended Indexed, use the parent class' hashed_models.
+      return superclass.hashed_models unless @is_indexed
+
+      @hashed_models ||= unique_fields.each_with_object({}) do |field, hash|
+        hash[field] = {}
+      end
+    end
+
     private
 
     def unique_fields
@@ -79,12 +111,6 @@ module ProductTaxonomy
           validator.is_a?(UniquenessValidator)
         end
       end.keys
-    end
-
-    def hashed_models
-      @hashed_models ||= unique_fields.each_with_object({}) do |field, hash|
-        hash[field] = {}
-      end
     end
   end
 end
