0ff1347ead7ce0fa710cb25c5721b7b8161377ee
Hadi Jafar <hadi.jafar@shopify.com>
2026-02-10 14:00:44 -0500

    updating attribute data

--- dev/test/integration/orphan_detection_test.rb
+++ dev/test/integration/orphan_detection_test.rb
@@ -4,15 +4,8 @@ require_relative "../test_helper"
 
 module ProductTaxonomy
   class OrphanDetectionTest < ActiveSupport::TestCase
-    parallelize(workers: 1)
-
-    @@taxonomy_loaded = false
-
     setup do
-      unless @@taxonomy_loaded
-        Command.new(quiet: true).load_taxonomy
-        @@taxonomy_loaded = true
-      end
+      ProductTaxonomy::Loader.load(data_path: ProductTaxonomy.data_path)
     end
 
     test "All values are referenced by at least one attribute" do
@@ -28,14 +21,38 @@ module ProductTaxonomy
     end
 
     test "All attributes belong to at least one category" do
+      # Attributes intentionally not assigned to any category. These are either:
+      # - Extended attributes that duplicate a base attribute already on the category
+      # - Attributes too specific for a parent category (would not apply to all children)
+      known_orphan_attributes = [
+        "accessory_material",
+        "arrow/bolt_material",
+        "binocular/monocular_design",
+        "book/file_cover_material",
+        "chair/sofa_features",
+        "coffee_filter_basket_size",
+        "diaper_type",
+        "disposable/reusable_item_material",
+        "door/frame_application",
+        "mat/rug_shape",
+        "paintball/airsoft_equipment_included",
+        "pet_apparel/bedding_features",
+        "popcorn_kernel_variety",
+        "pole/post_material",
+        "timepiece_features",
+        "washer/dryer_features",
+        "watch/band_material",
+      ].sort.freeze
+
       all_category_attributes = Category.all.flat_map(&:attributes).uniq
       orphan_attributes = Attribute.all.reject { |attr| all_category_attributes.include?(attr) }
 
       orphan_ids = orphan_attributes.map(&:friendly_id).sort
-      message = "Found #{orphan_ids.size} orphan attribute(s) not assigned to any category:\n" \
-                "  #{orphan_ids.join("\n  ")}"
+      unexpected_orphans = orphan_ids - known_orphan_attributes
+      message = "Found #{unexpected_orphans.size} unexpected orphan attribute(s) not assigned to any category:\n" \
+                "  #{unexpected_orphans.join("\n  ")}"
 
-      assert_empty orphan_ids, message
+      assert_empty unexpected_orphans, message
     end
   end
 end
